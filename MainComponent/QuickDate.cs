4b5d34ebebb040094d9209034f5aa50a374dbb06
duraipandians <duraipandians@PD512841>
2018-06-11 11:30:36 -0700

    Add project files.

---
+++ MainComponent/QuickDate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("MainComponentTest")]
+namespace MainComponent
+{
+    internal class QuickDate
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = GetCurrentDate();
+            Console.WriteLine($"Today's date is {now}");
+            Console.ReadLine();
+        }
+
+        internal static DateTime GetCurrentDate()
+        {
+            return DateTime.Now.Date;
+        }
+    }
+}
---
+++ MainComponentTest/DataDrivenTestfixture.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MainComponentTest
+{
+    [TestFixture(10)]
+    [TestFixture(42)]
+    public class DataDrivenTestFixture
+    {
+        int _x;
+
+        public DataDrivenTestFixture(int x)
+        {
+            _x = x;
+        }
+
+        [Test]
+        public void TestArguments()
+        {
+            Assert.Pass($"X is {_x}");
+        }
+    }
+}
